575a9bb97519ba10b3a39b4eca1d22bcbe40f31f
Cynacyn <32937411+Cynacyn@users.noreply.github.com>
2022-09-21 20:58:32 +0300

    Log::Logger::~Logger()

--- Log.cpp
+++ Log.cpp
@@ -1,7 +1,21 @@
 module Log;
 
 import <string>;
+import <iostream>;
+
+Log::Logger::~Logger()
+{
+    bool flush{ _messages.size() };
+    while (_messages.size())
+    {
+        std::cout << _messages.front() << "\n";
+        _messages.pop();
+    }
+    if (flush)
+        std::cout << std::endl;
+}
 
 void Log::Logger::log(std::string msg)
 {
+    _messages.emplace(std::move(msg));
 }
