6903b9827c9774976e55e49e2fa4a3a655307f3e
Cynacyn <32937411+Cynacyn@users.noreply.github.com>
2022-09-24 02:45:40 +0300

    Data and some diagrams

--- DataModel.cpp
+++ DataModel.cpp
@@ -2,6 +2,7 @@ module DataModel;
 
 import <optional>;
 import <string>;
+import <exception>;
 
 namespace Model
 {
@@ -9,16 +10,45 @@ namespace Model
     {
     public: 
         DataImpl() = default;
-        DataImpl(DataImpl&&) = default;
-        DataImpl& operator=(DataImpl&&) = default;
+        DataImpl(DataImpl&&) noexcept = default;
+        DataImpl& operator=(DataImpl&&) noexcept = default;
 
         DataImpl(std::string);
+
+    public:
+        std::string GetValueImpl() const;
+        bool HasValueImpl() const noexcept;
 
     private:
         std::optional< std::string > _value;
     };
 
-    Data::DataImpl::DataImpl(std::string)
+    Data::DataImpl::DataImpl(std::string value)
+        : _value{ std::move( value ) }
+    {}
+
+    std::string Data::DataImpl::GetValueImpl() const
     {
+        return _value.value();;
+    }
+
+    bool Data::DataImpl::HasValueImpl() const noexcept
+    {
+        return _value.has_value();
+    }
+    
+    /// 
+    /// 
+    /// 
+    ///
+    
+    std::string Data::GetString() const
+    {
+        return _impl->GetValueImpl();
+    }
+
+    bool Data::HasValue() const
+    {
+        return _impl->HasValueImpl();
     }
 }
--- DataModel.ixx
+++ DataModel.ixx
@@ -1,13 +1,18 @@
 export module DataModel;
 
 import <memory>;
+import <string>;
 
 export namespace Model
 {
     class Data
     {
     public:
+        // throws std::bad_optional_access if there's no value
+        std::string GetString() const;
 
+        // usually called before GetString()
+        bool HasValue() const;
 
     private:
         class DataImpl;
@@ -15,4 +20,5 @@ export namespace Model
     private:
         std::unique_ptr< DataImpl > _impl;
     };
+
 }
